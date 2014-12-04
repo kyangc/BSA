29af9ba16aab077e3d82a50e7b400a73d2756247
Dormitory-Laptop <kyangc@gmail.com>
2014-12-04 11:42:00 +0800

    修改：单TA定位算法。

--- BSASimulator/TA.cs
+++ BSASimulator/TA.cs
@@ -69,29 +69,86 @@ namespace BSASimulator
                     var posiblePostion = new Dictionary<int, List<double[]>>();
                     for (int i = 0; i < _dataProvider.GetTotalStep(); i++) posiblePostion.Add(i, new List<double[]>());
 
-                    for (int i = 0; i + 3 < _dataProvider.GetTotalStep(); i++)
+                    //Generate posible positions
+                    if (_option.GetIsUsingExternalData())
                     {
-                        UpdateProgress(i, _dataProvider.GetTotalStep());
-                        string cellid1 = _dataProvider.GetTaAt(i).Keys.ToArray()[0];
-                        string cellid2 = _dataProvider.GetTaAt(i + 3).Keys.ToArray()[0];
-                        if (
-                            !_dataProvider.GetTaAt(i).Keys.ToArray()[0].Equals(
-                                _dataProvider.GetTaAt(i + 3).Keys.ToArray()[0]) &&
-                            !_option.GetIsUsingExternalData())
+                        var baseInfo = new List<Dictionary<string, int[]>>();
+                        int StartIndex = 0,
+                            EndIndex = 0;
+                        string duringCellid = _dataProvider.GetTaAt(0).Keys.ToArray()[0];
+                        int currentBase = 0;
+                        var baseItem = new Dictionary<string, int[]>();
+                        baseItem.Add(duringCellid, new[] {0, 0});
+                        baseInfo.Add(baseItem);
+
+                        for (int i = 0; i < _dataProvider.GetTotalStep(); i++)
                         {
-                            cellid2 = _dataProvider.GetTaAt(i + 3).Keys.ToArray()[1];
+                            string currentCellId = _dataProvider.GetTaAt(i).Keys.ToArray()[0];
+
+                            if (currentCellId.Equals(duringCellid))
+                            {
+                                EndIndex = i;
+                                baseInfo[currentBase][currentCellId][0] = StartIndex;
+                                baseInfo[currentBase][currentCellId][1] = EndIndex;
+                            }
+                            else
+                            {
+                                StartIndex = i;
+                                EndIndex = i;
+                                currentBase++;
+                                duringCellid = currentCellId;
+                                baseItem = new Dictionary<string, int[]>();
+                                baseItem.Add(currentCellId, new[] {StartIndex, EndIndex});
+                                baseInfo.Add(baseItem);
+                            }
                         }
-                        double[] bsPosition1 = _dataProvider.GetBsPosition(cellid1);
-                        double[] bsPosition2 = _dataProvider.GetBsPosition(cellid2);
-                        double x = bsPosition1[0];
-                        double y = bsPosition1[1];
-                        double deltaX = (bsPosition2[0] - bsPosition1[0])/4;
-                        double deltaY = (bsPosition2[1] - bsPosition1[1])/4;
-                        for (int j = 0; j < 4; j++)
+
+                        for (int i = 0; i + 1 < baseInfo.Count; i++)
                         {
-                            posiblePostion[i + j].Add(new[] {x + deltaX*j, y + deltaY*j});
+                            Dictionary<string, int[]> info1 = baseInfo[i];
+                            Dictionary<string, int[]> info2 = baseInfo[i + 1];
+                            double[] position1 = _dataProvider.GetBsPosition(info1.Keys.ToArray()[0]);
+                            double[] position2 = _dataProvider.GetBsPosition(info2.Keys.ToArray()[0]);
+                            int stepStart = info1.Values.ToArray()[0][0];
+                            int stepEnd = info2.Values.ToArray()[0][1];
+                            int stepCount = stepEnd - stepStart + 1;
+                            double x = position1[0];
+                            double y = position1[1];
+                            double deltaX = (position2[0] - position1[0])/stepCount;
+                            double deltaY = (position2[1] - position1[1])/stepCount;
+                            for (int j = stepStart; j <= stepEnd; j++)
+                            {
+                                posiblePostion[j].Add(new[] {x + deltaX*(j - stepStart), y + deltaY*(j - stepStart)});
+                            }
                         }
                     }
+                    else
+                    {
+                        for (int i = 0; i + 3 < _dataProvider.GetTotalStep(); i++)
+                        {
+                            UpdateProgress(i, _dataProvider.GetTotalStep());
+                            string cellid1 = _dataProvider.GetTaAt(i).Keys.ToArray()[0];
+                            string cellid2 = _dataProvider.GetTaAt(i + 3).Keys.ToArray()[0];
+                            if (
+                                !_dataProvider.GetTaAt(i).Keys.ToArray()[0].Equals(
+                                    _dataProvider.GetTaAt(i + 3).Keys.ToArray()[0]) &&
+                                !_option.GetIsUsingExternalData())
+                            {
+                                cellid2 = _dataProvider.GetTaAt(i + 3).Keys.ToArray()[1];
+                            }
+                            double[] bsPosition1 = _dataProvider.GetBsPosition(cellid1);
+                            double[] bsPosition2 = _dataProvider.GetBsPosition(cellid2);
+                            double x = bsPosition1[0];
+                            double y = bsPosition1[1];
+                            double deltaX = (bsPosition2[0] - bsPosition1[0])/4;
+                            double deltaY = (bsPosition2[1] - bsPosition1[1])/4;
+                            for (int j = 0; j < 4; j++)
+                            {
+                                posiblePostion[i + j].Add(new[] {x + deltaX*j, y + deltaY*j});
+                            }
+                        }
+                    }
+
                     for (int i = 0; i < _dataProvider.GetTotalStep(); i++)
                     {
                         _resultPath.Add(GetAvgPosition(posiblePostion[i]));
